6dcdc9b33bf4f3d389cfc7dada5dc05ef9735bc4
vadimKapitan <xxxcap13@gmail.com>
2024-04-13 23:18:33 +0300

    Start

---
+++ src/test/java/pro/sky/firstrojectpring/FirstProjectSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.firstrojectpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstProjectSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
