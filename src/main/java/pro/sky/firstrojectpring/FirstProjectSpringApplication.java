70f751232a864a7a44b29830eb0e445a47959d47
vadimKapitan <xxxcap13@gmail.com>
2024-04-13 23:17:17 +0300

    Start

---
+++ src/main/java/pro/sky/firstrojectpring/FirstProjectSpringApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.firstrojectpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstProjectSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstProjectSpringApplication.class, args);
+	}
+
+}
